a61d50b89184e461bee3427969db0514b017cfb8
Eunice Lok <eunicelp511@gmail.com>
2020-05-30 22:40:45 +0800

    initial layout for card using web components

--- README.md
+++ README.md
@@ -11,6 +11,18 @@ Build a trello-like-app using **only** web components aside from vanilla HTML, C
 ```
 git clone https://github.com/prwhoeatsnonstop/trello-like-app.git
 cd trello-like-app
-open with live server using VS Code
+open with live server using Visual Studio Code
 ```
+
+##### Task lists
+- [x] display all columns with all cards
+- [] create a new card
+- [] modify a card
+- [] delete a card
+- [] add a column
+- [] modify a column
+- [] delete a column
+- [] search for any keywords presents on one or multiple cards. The view should update - [] without reloading the whole page
+- [] drag and drop a card from one column to another
+- [] click on a card to see its description. The description should be in the same view  and extend the card container. It should not open in another page or popup/modal
 
--- card.js
+++ card.js
@@ -4,14 +4,50 @@ let n =  new Date();
 let y = n.getFullYear();
 let m = n.getMonth() + 1;
 let d = n.getDate();
-document.getElementById("date").innerHTML = d + "/" + m + "/" + y;
+document.getElementById('date').innerHTML = d + '/' + m + '/' + y;
+
+
+const template = document.createElement('template');
+template.innerHTML = `
+<style>
+    h3 {
+        color: #172b4d;
+    }
+
+    .user-card {
+        font-family: 'Arial', sans-serif;
+        background: #f4f4f4;
+        width: 500px;
+        display: grid;
+        grid-template-columns: 1fr 2fr;
+        grid-gap: 10px;
+        margin-bottom: 15px;
+        border-bottom: darkorchid 5px solid;
+}
+</style>
+<div class='user-card'>
+    <h3></h3>
+    <div class='content'>
+    </div>
+    <button id="submit">Add card</button>
+</div>
+`;
 
 class UserCard extends HTMLElement {
     constructor(){
         super();
-        this.innerHTML = `${this.getAttribute('name')}`;
+        // attaching shadow to this custom element
+        this.attachShadow({ mode: 'open'})
+        this.shadowRoot.appendChild(template.content.
+            cloneNode(true));
+        // this.innerHTML = `${this.getAttribute('title')}`;
+        this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
+        // this.shadowRoot.querySelector('p').innerText = this.getAttribute('content');
     }
 }
 
 window.customElements.define('user-card', UserCard);
+
+
+
 
